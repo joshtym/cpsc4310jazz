490cb6e8a1b497119edb3e53c1269b1763220d2e
jtymburski <joshtym@users.noreply.github.com>
2024-07-26 15:42:17 -0600

    a4q3 getting places

--- a4/a4q3.py
+++ a4/a4q3.py
@@ -1,4 +1,5 @@
 import nltk
+import math
 
 # metrics to tokenize and lemmatize all the lowercase words and return it as a list
 def tokenizeAndClean(text):
@@ -11,16 +12,59 @@ def tokenizeAndClean(text):
 def jaccard_score(label1, label2):
     return (float(len(label1.intersection(label2))))/float(len(label1.union(label2)))
 
+def getCountVector(terms, tokenizedList):
+   countValues = []
+   for term in terms:
+      count = 0
+      for token in tokenizedList:
+         if token == term:
+            count += 1
+      countValues.append(count)
+   return countValues
+
+def getDict(terms,countVector):
+   count = 0
+   dictionary = {}
+   for word in terms:
+      dictionary[word] = countVector[count]
+      count += 1
+   return dictionary
+
+def normalizeVec(vec):
+   num = 0.0
+   for i in vec:
+      num += i*i
+   num = math.sqrt(num)
+   newVec = vec
+
+   for i in range(0,len(vec)):
+      newVec[i] = vec[i] / num
+
+   return newVec
+
+def getCosineSim(listOne, listTwo, allTerms):
+   countOne = normalizeVec(getCountVector(allTerms, listOne))
+   countTwo = normalizeVec(getCountVector(allTerms, listTwo))
+   cosineVal = 0.0
+   for i in range(0,len(countOne)):
+      cosineVal += (countOne[i] * countTwo[i])
+   return cosineVal
+
 def mainFunc():
    fileOne = open('test.txt')
    fileTwo = open('test2.txt')
-   tokenizedFiles = []
-   firstSet = set(tokenizeAndClean(fileOne.read()))
-   secondSet = set(tokenizeAndClean(fileTwo.read()))
-   tokenizedFiles.append(firstSet)
-   tokenizedFiles.append(secondSet)
+   tokenizedListOne = tokenizeAndClean(fileOne.read())
+   tokenizedListTwo = tokenizeAndClean(fileTwo.read())
+   firstSet = set(tokenizedListOne)
+   secondSet = set(tokenizedListTwo)
+
+   #dictOne = getDict(allTerms,countOne)
+   #dictTwo = getDict(allTerms,countTwo)
 
    # Get Jaccard Score
-   print("Jaccard Similarity is: " + str(jaccard_score(tokenizedFiles[0], tokenizedFiles[1])))
+   print("Jaccard Similarity is: " + str(jaccard_score(firstSet, secondSet)))
+
+   # Get Cosine Similarity
+   print("Cosine Similarity is: " + str(getCosineSim(tokenizedListOne, tokenizedListTwo, firstSet.union(secondSet))))
 
 mainFunc()
